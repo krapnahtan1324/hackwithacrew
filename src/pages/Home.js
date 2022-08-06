c4306c15c86b06c663e3a1ca456629b65fbf57e9
Nathan Park <ngp7ce@virginia.edu>
2022-08-06 19:35:19 -0400

    added to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,12 @@ import React from "react";
 
 const Home = () => {
     return (
-        <h1>Home</h1>
+        <div className="home txt">
+            <h1>Check up on your friends!</h1>
+            <h2>Already have an account?</h2>
+            <button className="click">Sign in</button>
+            <a href="">Or create one if not</a>
+        </div>
     );
 }
 
